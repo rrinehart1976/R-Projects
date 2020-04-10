8bb43a9adec25ee64070f5315839e1f1d771de75
Andrew Borst <aborst@iu.edu>
2020-04-10 12:04:24 -0400

    Create WeeklyCasesGraph.R

---
+++ WeeklyCasesGraph.R
@@ -0,0 +1,51 @@
+##:::::::::::::::::::::::::::::::::
+##  Graphs weekly cases counts and average cabinet price.
+##  Andrew Borst 
+##  Last Update 4/10/20
+##:::::::::::::::::::::::::::::::::
+
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(reshape2)
+library(odbc)
+
+#setwd(paste(getwd(), "/R", sep = ""))
+
+con <- dbConnect(odbc(),
+                 Driver = "SQL Server",
+                 Server = "IN-SQL01",
+                 Database = "COIN",
+                 UID = "userName",
+                 PWD = "password", #rstudioapi::askForPassword(),
+                 Port = 1433)
+
+v <- dbGetQuery(con, "SELECT * FROM vwReportKeyData")
+
+v$SubmittedDate <- as.Date(v$SubmittedDate)
+
+caseorders <- v %>% 
+    select(SubmittedDate, OrderCaseTotal, AdjustPrice, OrderTotal, Brand, ProductLine, Overlay) %>% 
+    filter(OrderCaseTotal > 0) %>%
+    filter(OrderCaseTotal != "NULL") %>%  
+    filter(year(SubmittedDate) == 2020) %>% 
+    mutate(OrderCaseTotal = as.integer(OrderCaseTotal)) %>% 
+    mutate(weekNo = case_when(is.na(SubmittedDate) ~ 99, TRUE ~ isoweek(SubmittedDate))) %>%
+    mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) %>% 
+    mutate(NetPrice = OrderTotal + AdjustPrice)
+
+weeklyBrand <- caseorders %>% 
+    group_by(weekNo, Brand) %>% 
+    summarise(Cases = sum(OrderCaseTotal), CaseAverage = sum(NetPrice)/sum(OrderCaseTotal)) 
+
+weeklyBoth <- caseorders %>% 
+  group_by(weekNo) %>% 
+  summarise(Cases = sum(OrderCaseTotal), CaseAverage = sum(NetPrice)/sum(OrderCaseTotal)) %>% 
+  mutate(Brand = "Both")  
+  
+
+ggplot(weeklyBrand) +
+  geom_bar(aes(weekNo, Cases, fill=Brand), stat="identity") +
+  geom_line(aes(weekNo, CaseAverage, color=Brand)) +
+  geom_line(data=weeklyBoth, aes(weekNo, CaseAverage))
+
